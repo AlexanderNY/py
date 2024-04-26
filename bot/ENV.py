ec2537d2b02894c8e6ab8eced7cbe0df93867578
Alexander <pulchrum@yandex.ru>
2024-04-26 20:00:42 +0300

    обновление бота

---
+++ bot/ENV.py
@@ -0,0 +1,68 @@
+
+#database
+
+db_name='db_bot'
+db_user='postgres' 
+db_password='1qaz!QAZ'
+db_host='localhost'
+
+#proxy
+PROXY_HOST = '38.154.89.230'  # rotating proxy or host
+PROXY_PORT = 8000 # port
+PROXY_USER = 'nSMJ7N' # username
+PROXY_PASS = 'HQhCVD' # password
+
+#selenium
+DRIVER_PATH='K:\Project\chromedriver-win64\chromedriver.exe'
+
+#telethon
+api_id = 15723016
+api_hash = 'fd10c198eaa94bc4fe3f82415eb46ee6'
+
+#twitter
+tw_username="Alexander_Nepl"    
+tw_password="twLehyjqgbplsvkfltytw!"
+
+#wordpress
+#wordpress_user = "Alexander_N"
+#wordpress_password = '18R4 6MBF sriL 48GA Od0G GP0P'
+#wordpress_credentials = wordpress_user + ":" + wordpress_password
+#wordpress_token = base64.b64encode(wordpress_credentials.encode())
+#wordpress_header = {'Authorization': 'Basic ' + wordpress_token.decode('utf-8')}
+#
+#api_url = 'https://www.nepluev.info/wp-json/wp/v2/posts/'
+#api_url_page = 'https://www.nepluev.info/wp-json/wp/v2/pages/'
+
+#path to bot foder
+path_to_bot='K:/Project/py/bot'
+
+
+#Done! Congratulations on your new bot. You will find it at t.me/Pulchrum_collect_bot. You can now add a description, about section and profile picture for your bot, see /help for a list of commands. By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it. Just make sure the bot is fully operational before you do this.
+#
+#Use this token to access the HTTP API:
+#6951168264:AAGaXyK4Bi3jiyZHm8yiN4LI6cg2UZ0pPV0
+#Keep your token secure and store it safely, it can be used by anyone to control your bot.
+
+
+
+#API Key = 
+#
+#lPVn9MM3L6oUBQSrKsepnoinQ
+#
+#API Key Secret
+#
+#FmmVTgG593mqC3LFE7Ok2hz5wDVXmRoAKlVpYpJXvGPp1c0dJG
+#
+#Bearer Token
+#AAAAAAAAAAAAAAAAAAAAAHlZsAEAAAAA8r%2FVfWCFa%2FsG2BdOxh11jnxmsVA%3Dw2npInOFFhkdRrXCCCz8XiK6PNnho7HQjtt98tqFDI48HZxcZg
+#
+#Access Token
+#1750586937998483456-pZ1YcyxGbLSrh1JAB9INsaig2EL2MT
+#
+#Access Token Secret
+#eQ6SpObaagjMuNzE7HAmcBfrj1ZyaeBlQnUxh12yJ5mpK
+
+#1750611460474957824Alexander_N
+#https://developer.twitter.com/en/portal/projects/1750611478988611585/apps/28334457/keys
+
+#twLehyjqgbplsvkfltytw!
